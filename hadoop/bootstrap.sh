9403bbfee6ba4341cc45ff4a5303f10e5012a57a
Gianluca Giorgio <gianluca.giorgio@meetrics.com>
2017-10-13 17:05:31 +0200

    Made file copy to HDFS more reliable (still need to be improved)

--- hadoop/bootstrap.sh
+++ hadoop/bootstrap.sh
@@ -12,10 +12,24 @@ cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; 
 # altering the core-site configuration
 sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
 
-
 service ssh start
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
+
+# Try to copy all the files in the mounted $HDFS_UPLOAD volume to hdfs root
+echo "FIX ME! If the volume folder is empty it will NEVER exit the while here!"
+echo -n "Copy $HDFS_UPLOAD files into HDFS"
+while true; do
+  hdfs dfs -put $HDFS_UPLOAD/* / 2> /dev/null
+  if [ $? -eq 0 ]; then
+	  break
+  fi
+  echo -n "."
+  sleep 1
+done
+echo "OK!"
+
+
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
@@ -24,6 +38,3 @@ fi
 if [[ $1 == "-bash" ]]; then
   /bin/bash
 fi
-
-# Copy all the files in the mounted "hdfs-uploaded" volume to hdfs (where?)
-hdfs dfs -put /hdfs-uploaded/* /
---
+++ hadoop/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t gia90/hadoop_debian:2.7.1 .
